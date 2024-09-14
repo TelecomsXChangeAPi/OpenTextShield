2e83751d6c3fc371a7b2e2f1455a4e8ab007c2a4
ajamous <a.jamous@telecomsxchange.com>
2024-09-13 22:48:51 -0400

    #1 Check py environment on start, create if does not exist

--- start.sh
+++ start.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+# Exit the script if any command fails
+set -e
+
 # Display the CURL command to the user
 echo "To test the OpenTextShield API, you can run the following curl command:"
 echo 'curl -X POST "http://localhost:8002/predict/" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"text\":\"Your SMS content here\",\"model\":\"bert\"}"'
 
-# Activate the ots virtual environment (correct path for local testing)
-source /home/ots/ots/bin/activate
+# Set the virtual environment path
+VENV_PATH="/home/ots/OpenTextShield/ots"
+
+# Check if the virtual environment exists, and if not, create it
+if [ ! -d "$VENV_PATH" ]; then
+  echo "Virtual environment not found. Creating the virtual environment..."
+  python3.12 -m venv $VENV_PATH
+  
+  echo "Virtual environment created. Installing dependencies..."
+  source $VENV_PATH/bin/activate
+  pip install --upgrade pip
+  pip install -r /home/ots/OpenTextShield/requirements.txt
+else
+  echo "Virtual environment found."
+  # Activate the ots virtual environment
+  source $VENV_PATH/bin/activate
+fi
 
 # Start the API service using Uvicorn (correct module path)
+echo "Starting the OpenTextShield API..."
 uvicorn src.api-interface.api_2_mBERT:app --host 0.0.0.0 --port 8002
 
